5b8b3ef76e6530ac1ece7024ebcdddbe230516de
Mohammad <mohammad.jalili.torkamani@gmail.com>
2023-07-02 18:38:37 +0330

    added assertions to the run method

--- src/main/java/org/jenkinsci/plugins/sshsteps/steps/PutStep.java
+++ src/main/java/org/jenkinsci/plugins/sshsteps/steps/PutStep.java
@@ -22,96 +22,91 @@ import org.kohsuke.stapler.DataBoundSetter;
  */
 public class PutStep extends BasicSSHStep {
 
-  private static final long serialVersionUID = 9183111587222550149L;
+	private static final long serialVersionUID = 9183111587222550149L;
 
-  @Getter
-  private final String from;
+	@Getter
+	private final String from;
 
-  @Getter
-  private final String into;
+	@Getter
+	private final String into;
 
-  @Getter
-  @Setter
-  @DataBoundSetter
-  private String filterBy = "name";
+	@Getter
+	@Setter
+	@DataBoundSetter
+	private String filterBy = "name";
 
-  @Getter
-  @Setter
-  @DataBoundSetter
-  private String filterRegex;
+	@Getter
+	@Setter
+	@DataBoundSetter
+	private String filterRegex;
 
-  @DataBoundConstructor
-  public PutStep(String from, String into) {
-    this.from = from;
-    this.into = into;
-  }
+	@DataBoundConstructor
+	public PutStep(String from, String into) {
+		this.from = from;
+		this.into = into;
+	}
 
-  @Override
-  public StepExecution start(StepContext context) throws Exception {
-    return new Execution(this, context);
-  }
+	@Override
+	public StepExecution start(StepContext context) throws Exception {
+		return new Execution(this, context);
+	}
 
-  @Extension
-  public static class DescriptorImpl extends SSHStepDescriptorImpl {
+	@Extension
+	public static class DescriptorImpl extends SSHStepDescriptorImpl {
 
-    @Override
-    public String getFunctionName() {
-      return "sshPut";
-    }
+		@Override
+		public String getFunctionName() {
+			return "sshPut";
+		}
 
-    @Override
-    public String getDisplayName() {
-      return getPrefix() + getFunctionName() + " - Put a file/directory on remote node.";
-    }
-  }
+		@Override
+		public String getDisplayName() {
+			return getPrefix() + getFunctionName() + " - Put a file/directory on remote node.";
+		}
+	}
 
-  public static class Execution extends SSHStepExecution {
+	public static class Execution extends SSHStepExecution {
 
-    private static final long serialVersionUID = -4497192469254138827L;
+		private static final long serialVersionUID = -4497192469254138827L;
 
-    protected Execution(PutStep step, StepContext context)
-        throws IOException, InterruptedException {
-      super(step, context);
-    }
+		protected Execution(PutStep step, StepContext context) throws IOException, InterruptedException {
+			super(step, context);
+		}
 
-    @Override
-    protected Object run() throws Exception {
-      PutStep step = (PutStep) getStep();
-      FilePath ws = getContext().get(FilePath.class);
-      assert ws != null;
-      FilePath fromPath;
+		@Override
+		protected Object run() throws Exception {
+			PutStep step = (PutStep) getStep();
+			FilePath ws = getContext().get(FilePath.class);
+			assert ws != null : "FilePath is null";
+			FilePath fromPath;
+			if (Util.fixEmpty(step.getFrom()) == null) {
+				throw new IllegalArgumentException("from is null or empty");
+			}
+			fromPath = ws.child(step.getFrom());
+			assert fromPath != null : "fromPath is null";
+			if (!fromPath.exists()) {
+				throw new IllegalArgumentException(fromPath.getRemote() + " does not exist.");
+			}
+			if (Util.fixEmpty(step.getInto()) == null) {
+				throw new IllegalArgumentException("into is null or empty");
+			}
+			return getChannel().call(new PutCallable(step, getListener(), fromPath.getRemote()));
+		}
 
-      if (Util.fixEmpty(step.getFrom()) == null) {
-        throw new IllegalArgumentException("from is null or empty");
-      }
+		private static class PutCallable extends SSHMasterToSlaveCallable {
 
-      fromPath = ws.child(step.getFrom());
+			private final String from;
 
-      if (!fromPath.exists()) {
-        throw new IllegalArgumentException(fromPath.getRemote() + " does not exist.");
-      }
+			public PutCallable(PutStep step, TaskListener listener, String from) {
+				super(step, listener);
+				this.from = from;
+			}
 
-      if (Util.fixEmpty(step.getInto()) == null) {
-        throw new IllegalArgumentException("into is null or empty");
-      }
-
-      return getChannel().call(new PutCallable(step, getListener(), fromPath.getRemote()));
-    }
-
-    private static class PutCallable extends SSHMasterToSlaveCallable {
-
-      private final String from;
-
-      public PutCallable(PutStep step, TaskListener listener, String from) {
-        super(step, listener);
-        this.from = from;
-      }
-
-      @Override
-      public Object execute() {
-        final PutStep step = (PutStep) getStep();
-        return getService().put(from, step.getInto(), step.getFilterBy(), step.getFilterRegex());
-      }
-    }
-  }
+			@Override
+			public Object execute() {
+				final PutStep step = (PutStep) getStep();
+				return getService().put(from, step.getInto(), step.getFilterBy(), step.getFilterRegex());
+			}
+		}
+	}
 }
